c72bc93fa50e4be771fd1ca4a24183fdcd5351b1
Shubh-Nawani <shubhnawani@outlook.com>
2024-11-12 11:14:10 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
+import logo from "./Screenshot 2024-11-11 124828.png";
 import './App.css';
 
 function App() {
+  let shubh = "https://www.linkedin.com/in/shubhnawani"
   return (
-    <div className="App">
+    
+    <div className="App1">
+               React
+      
       <header className="App-header">
+        SHUBH NAWANI
+        <a href={shubh} target="same" rel="noopener noreferrer">click me</a>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="mailto:shubhnawani@outlook.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Made with ❤️ by Shubh Nawani.
         </a>
       </header>
     </div>
